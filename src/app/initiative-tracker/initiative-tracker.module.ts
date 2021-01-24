668212ffa75851998f8ab3a2b365b8d5d351accb
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-01-24 21:18:36 +0000

    Add material shared module

--- src/app/initiative-tracker/initiative-tracker.module.ts
+++ src/app/initiative-tracker/initiative-tracker.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { InitiativeTrackerRoutingModule } from './initiative-tracker-routing.module';
 import { InitiativeTrackerComponent } from './initiative-tracker.component';
+import { MaterialModule } from '../shared/material/material.module';
 
 
 @NgModule({
   declarations: [InitiativeTrackerComponent],
   imports: [
     CommonModule,
-    InitiativeTrackerRoutingModule
+    InitiativeTrackerRoutingModule,
+    MaterialModule
   ]
 })
 export class InitiativeTrackerModule { }
