ee693402f8b210d6459ac975d68ae6d70d654762
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-02-02 10:00:41 +0000

    Tide up

--- src/app/initiative-tracker/initiative-tracker-routing.module.ts
+++ src/app/initiative-tracker/initiative-tracker-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InitiativeTrackerComponent } from './initiative-tracker.component';
+import { InitiativeTrackerComponent } from './page/initiative-tracker.component';
 
 const routes: Routes = [{ path: '', component: InitiativeTrackerComponent }];
 
