569b59d874a7dd930369f18c7c26497932e36bef
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-02-02 10:02:25 +0000

    Implement Initiative Tracker component

---
+++ src/app/initiative-tracker/page/initiative-tracker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-initiative-tracker',
+  templateUrl: './initiative-tracker.component.html',
+  styleUrls: ['./initiative-tracker.component.scss']
+})
+export class InitiativeTrackerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
