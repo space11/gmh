18f341490d28c35e47f78a2db91d0e81d36052c0
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-02-02 10:02:37 +0000

    Implement Initiative Tracker module

--- src/app/initiative-tracker/initiative-tracker.module.ts
+++ src/app/initiative-tracker/initiative-tracker.module.ts
@@ -2,16 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InitiativeTrackerRoutingModule } from './initiative-tracker-routing.module';
-import { InitiativeTrackerComponent } from './initiative-tracker.component';
+import { InitiativeTrackerComponent } from './page/initiative-tracker.component';
 import { MaterialModule } from '../shared/material/material.module';
+import { CreateActorComponent } from './components/create-actor/create-actor.component';
+import { CoreModule } from '../shared/core/core.module';
+import { ListOfActorsComponent } from './components/list-of-actors/list-of-actors.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
-  declarations: [InitiativeTrackerComponent],
+  declarations: [InitiativeTrackerComponent, CreateActorComponent, ListOfActorsComponent],
   imports: [
     CommonModule,
     InitiativeTrackerRoutingModule,
-    MaterialModule
+    MaterialModule,
+    CoreModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class InitiativeTrackerModule { }
